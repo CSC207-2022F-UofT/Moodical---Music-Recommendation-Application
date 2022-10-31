dd636a3d0ea424497fc0318cc5a03121c0b9d451
yumnarefai <yumnarefai@mail.utoronto.ca>
2022-10-31 17:15:17 -0400

    Defined a class entity named questionPool

--- src/main/java/questionPool.java
+++ src/main/java/questionPool.java
@@ -1,6 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuestionPool
-{
-    public static void main(String[] args) {
-        System.out.println("hello");
+{ List<String> lstOfQs = new ArrayList<>();
     }
-}
+
