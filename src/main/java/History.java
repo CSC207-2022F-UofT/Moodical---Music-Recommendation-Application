d625d6597a55fcdfc1c3b7507a2854cebf65597a
mqg88 <meghan.george@mail.utoronto.ca>
2022-10-31 17:48:01 -0400

    created favourites subclass created history class Added generate rec function to History

--- src/main/java/History.java
+++ src/main/java/History.java
@@ -1,6 +1,19 @@
 import java.util.ArrayList;
 
-public class History {
-ArrayList previous_songs;
+public class History extends Reccommendable {
+    public ArrayList previous_songs;
 
+    public History(){
+    this.previous_songs = new ArrayList<String>();
+    }
+    public String[] generateRec(ArrayList<String> favourites){
+        ArrayList<String> new_playlist = new ArrayList<String>();
+        for (int i = 0; i < 10; i++){
+            int index = (int) ((Math.random() * ((favourites.size()-1)));
+            String song = favourites.get(index);
+            if (!(new_playlist.contains(song))){
+                new_playlist.add(song);
+            }
+        }
+    }
 }
