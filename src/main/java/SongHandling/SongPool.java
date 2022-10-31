d62cba5097af5cfdbee5264c630b81285fffff43
arvin <arvin.gingoyon@outlook.com>
2022-10-31 17:27:14 -0400

    - finished SongPool - fixed issue with accidental deletion of last branch

---
+++ src/main/java/SongHandling/SongPool.java
@@ -0,0 +1,15 @@
+package SongHandling;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class SongPool {
+    public ArrayList<String> songs;
+
+    public SongPool() {
+        this.songs = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
