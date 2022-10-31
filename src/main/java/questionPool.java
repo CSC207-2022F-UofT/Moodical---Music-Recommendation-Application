1679da00e97644cf23f154aff655d9e95694afa5
yumnarefai <yumnarefai@mail.utoronto.ca>
2022-10-31 17:29:38 -0400

    Defined a class entity named questionPool

--- src/main/java/questionPool.java
+++ src/main/java/questionPool.java
@@ -1,7 +1,17 @@
+/* Below is the Question Pool entity object with its constructor.
+
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class QuestionPool
-{ List<String> lstOfQs = new ArrayList<>();
+
+
+public class QuestionPool {
+    List<String> lstOfQs = new ArrayList<>();
 }
 
+/*
+This class only consists of a list of strings.
+We will store and save questions from a csv file using interface which is another doc
+ */
