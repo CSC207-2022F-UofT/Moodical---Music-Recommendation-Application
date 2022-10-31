38dd63959925992bbb6323ba1968215988d96d83
yumnarefai <yumnarefai@mail.utoronto.ca>
2022-10-31 17:00:18 -0400

    Defined a class entity named questionPool

---
+++ src/main/java/questionPool.java
@@ -0,0 +1,15 @@
+
+/* This file defines a class named QuestionPool.
+
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class questionPool {
+    public List<String> lstOfQ = new ArrayList<>();
+}
+
+
+// leave the read csv and translating the questions from a document file into lst of strings to another class
+
