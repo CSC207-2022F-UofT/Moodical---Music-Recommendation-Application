e237970253dab665f57182fb6bd12f7e6aa73fdd
Savanna <savannapan@gmail.com>
2022-11-02 15:50:42 -0400

    Commit

---
+++ src/main/java/Processors/AnalysePlaylistProcessing.java
@@ -0,0 +1,10 @@
+package Processors;
+
+public class AnalysePlaylistProcessing {
+    /**
+     * Analyse the user's existing playlists and generate insights based on the songs
+     * in the playlist.
+     */
+
+
+}
