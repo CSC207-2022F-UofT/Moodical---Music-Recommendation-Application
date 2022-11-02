e118ca4fdbc9d982e63286f41fe5ad5e56d68343
mqg88 <meghan.george@mail.utoronto.ca>
2022-11-02 15:57:59 -0400

    added generaterec

--- src/main/java/History.java
+++ src/main/java/History.java
@@ -1,19 +1,23 @@
 import java.util.ArrayList;
 
-public class History extends Reccommendable {
-    public ArrayList previous_songs;
+public class History implements Recommendable {
+    public ArrayList<String> previous_songs;
 
     public History(){
     this.previous_songs = new ArrayList<String>();
     }
-    public String[] generateRec(ArrayList<String> favourites){
+    @Override
+    public ArrayList<String> recommend(){
         ArrayList<String> new_playlist = new ArrayList<String>();
         for (int i = 0; i < 10; i++){
-            int index = (int) ((Math.random() * ((favourites.size()-1)));
-            String song = favourites.get(index);
+            int index = (int) ((Math.random() * ((this.previous_songs.size()-1)));
+            String song = this.previous_songs.get(index);
             if (!(new_playlist.contains(song))){
                 new_playlist.add(song);
             }
         }
+        return new_playlist;
     }
+
+
 }
--- src/main/java/History_Controller.java
+++
@@ -1,2 +0,0 @@
-public class History_Controller {
-}
