590d2699bac44e5cab665b979167a07345e78124
Arvin <arvin.gingoyon@outlook.com>
2022-11-09 16:20:12 -0500

    - Deleted CSVFile entity (not useful) - Finished implementation of CSVFileProcessing - Fixed ReadableFile interface (i.e. implemented ReadRows properly) - Finished implementation of SongPool entity

--- src/main/java/Entities/ReadableFile.java
+++ src/main/java/Entities/ReadableFile.java
@@ -7,5 +7,5 @@ public interface ReadableFile {
      * Read the file type specified
      **/
 
-     ArrayList<String> readFile(); // Returns an array list containing String for each line of file (i.e. csv, etc.)
+     ArrayList<String> readRows(String filePath); // Returns an array list containing String for each line of file (i.e. csv, etc.)
 }
