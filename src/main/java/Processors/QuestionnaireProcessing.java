f2631cdad917cba8731742040a5f7d0211263d9b
yumnarefai <yumnarefai@mail.utoronto.ca>
2022-11-11 19:22:38 -0500

    questionprocessing tasks

--- src/main/java/Processors/QuestionnaireProcessing.java
+++ src/main/java/Processors/QuestionnaireProcessing.java
@@ -11,27 +11,27 @@ import java.util.Random;
 
 import static jdk.nashorn.internal.runtime.ScriptingFunctions.readLine;
 
-public class QuestionnaireProcessing implements ReadableFile {
+public class QuestionnaireProcessing {
     /**
      * This class has two methods for processing - obtaining the list of questions from Questions.csv and then
      * filtering out this list to obtain 5 questions at random for higher level usage.
      *
-     * @param args containing processed CSV line from questions.csv
      * @return
      */
-    public static ArrayList<String> main (){
+
+    public static ArrayList<String[]> csv_reader (){
 
     // this method takes the questions csv file and returns list of questions of type String
 
         String path = "questions.csv";
         String line = "";
-        ArrayList<String> questions = new ArrayList<String>();
+        ArrayList<String[]> questions = new ArrayList<>();
 
         try {BufferedReader br = new BufferedReader(new FileReader(path));
-
             while ((line = br.readLine()) != null){
                 String[] values = line.split(", ");
-                questions.add(readLine(values));
+                questions.add(values);
+
             }
 
     } catch (FileNotFoundException error) {
@@ -42,13 +42,14 @@ public class QuestionnaireProcessing implements ReadableFile {
         return questions;
     }
 
-    public static ArrayList<String> getRandQues(ReadableFile p){
+    public static ArrayList<String[]> getRandQues(ReadableFile p){
 
-        // this method takes from a list of questions of type string and returns a list of 5 random questions.
+        // this method takes from a list of list questions of type string[] and
+        // returns a list of 5 random questions which are of type list.
         // the value at chosen index is deleted to avoid repetitions of questions
 
-        ArrayList<String> lst = p.readFile();
-        ArrayList<String> filteredLst = new ArrayList<>();
+        ArrayList<String[]> lst = csv_reader();
+        ArrayList<String[]> filteredLst = new ArrayList<>();
         Random rand = new Random();
         for (int i = 0; i <= lst.size(); i++){
             int randIndex = rand.nextInt(lst.size());
@@ -56,12 +57,12 @@ public class QuestionnaireProcessing implements ReadableFile {
             lst.remove(randIndex);
 
         }
-        return (ArrayList<String>) filteredLst.subList(0,5);
+        return (ArrayList<String[]>) filteredLst.subList(0,5);
     }
-    @Override
-    public ArrayList<String> readFile() {
-        return main();
-    }
+//    @Override
+//    public ArrayList<String[]> readFile() {
+//        return csv_reader();
+//    }
 
 
 
