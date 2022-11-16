ddc5775332f7d5c0d6b9ee78365523aa0fc75177
yumnarefai <yumnarefai@mail.utoronto.ca>
2022-11-16 16:34:41 -0500

    Had to redo processing because it was not saved

--- src/main/java/Entities/ReadableFile.java
+++ src/main/java/Entities/ReadableFile.java
@@ -7,5 +7,5 @@ public interface ReadableFile {
      * Read the file type specified
      **/
 
-     ArrayList<String> readRows(String filePath); // Returns an array list containing String for each line of file (i.e. csv, etc.)
+     ArrayList<String> readRows(Object filePath); // Returns an array list containing String for each line of file (i.e. csv, etc.)
 }
