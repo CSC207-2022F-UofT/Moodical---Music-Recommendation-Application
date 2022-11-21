c9539c0deb561210e180feebbc8a850995d153ce
minha-faheem <minha.faheem@mail.utoronto.ca>
2022-11-21 18:36:40 -0500

    Modified Account entity

--- src/main/java/Entities/Account.java
+++ src/main/java/Entities/Account.java
@@ -22,9 +22,6 @@ public class Account {
         this.userFavourites = favourites;
     }
 
-    public Account() {
-    }
-
     public void setUsername(String username){
         this.username = username;
     }
