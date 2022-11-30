ce264b0c625bcd333c453788436b04c5b011bc7b
yumnarefai <yumnarefai@mail.utoronto.ca>
2022-11-29 23:31:48 -0500

    Commit and made other tests

--- src/main/java/Processors/QuestionnaireProcessing.java
+++ src/main/java/Processors/QuestionnaireProcessing.java
@@ -3,7 +3,6 @@ package Processors;
 import Entities.ReadableFile;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class QuestionnaireProcessing {
--- src/test/java/processortests/QuestionnaireProcessingTest.java
+++ src/test/java/processortests/QuestionnaireProcessingTest.java
@@ -1,7 +1,6 @@
 package processortests;
 
 import Entities.ReadableFile;
-import Entities.questionPool;
 import Processors.CSVFileProcessing;
 import Processors.QuestionnaireProcessing;
 import org.junit.jupiter.api.AfterEach;
@@ -12,29 +11,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class QuestionnaireProcessingTest {
     ReadableFile p;
     String filepath;
-
     @BeforeEach
     void createExampleData() {
         filepath = "dataset/questions.csv";
         p = new CSVFileProcessing();
 
-        String exampleQ1 = "I am feeling optimistic about life at the moment";
-        String exampleQ2 = " My energy levels are adequate today";
-        String exampleQ3 = "My social battery is strong today";
-        String exampleQ4 = "My self-esteem is doing alright";
-        String exampleQ5 = "I have someone to talk to when I need it";
-
-        questionPool.getRandQ().add(exampleQ1);
-        questionPool.getRandQ().add(exampleQ2);
-        questionPool.getRandQ().add(exampleQ3);
-        questionPool.getRandQ().add(exampleQ4);
-        questionPool.getRandQ().add(exampleQ5);
-
     }
 
     @Test
     void getRandQuesByReadable(){
-        assertEquals(QuestionnaireProcessing.getRandQues(p, filepath).size(), 5);
+        assertEquals(QuestionnaireProcessing.getRandQues(p, filepath).size(), 5 );
 
     }
 
